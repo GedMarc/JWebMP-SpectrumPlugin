5e70a3ec884633ef316634c229e3dbfb8942109d
GedMarc <ged_marc@hotmail.com>
2018-07-23 19:34:47 +0200

    JDK 10 Service Loading rather than Classpath Search (Much Faster), Build update to classify JDK 10 dependencies properly

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,3 +1,6 @@
+import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.plugins.spectrum.colourpicker.JQSpectrumPageConfigurator;
+
 module com.jwebmp.plugins.spectrum.colourpicker {
 
 	exports com.jwebmp.plugins.spectrum.colourpicker;
@@ -8,4 +11,7 @@ module com.jwebmp.plugins.spectrum.colourpicker {
 
 	requires java.validation;
 	requires java.logging;
+
+	provides IPageConfigurator with JQSpectrumPageConfigurator;
+
 }
--- src/main/java/com/jwebmp/plugins/spectrum/colourpicker/JQSpectrumPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/spectrum/colourpicker/JQSpectrumPageConfigurator.java
@@ -17,8 +17,8 @@
 package com.jwebmp.plugins.spectrum.colourpicker;
 
 import com.jwebmp.core.Page;
-import com.jwebmp.core.PageConfigurator;
 import com.jwebmp.core.plugins.PluginInformation;
+import com.jwebmp.core.services.IPageConfigurator;
 
 /**
  * @author GedMarc
@@ -39,11 +39,8 @@ import com.jwebmp.core.plugins.PluginInformation;
 		pluginIconImageUrl = "bower_components/spectrum/example.png",
 		pluginLastUpdatedDate = "2017/03/04")
 public class JQSpectrumPageConfigurator
-		extends PageConfigurator
+		implements IPageConfigurator
 {
-
-	private static final long serialVersionUID = 1L;
-
 	@Override
 	public Page configure(Page page)
 	{
