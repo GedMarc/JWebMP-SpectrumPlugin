dc1307914969bd3bfa8e2364cd07f9e35e9eecb3
GedMarc <ged_marc@hotmail.com>
2020-08-25 01:16:21 +0200

    All version updates

--- jar-resources/META-INF/resources/bower_components/spectrum/i18n/jquery.spectrum-fa.js
+++ jar-resources/META-INF/resources/bower_components/spectrum/i18n/jquery.spectrum-fa.js
@@ -2,13 +2,17 @@
 // Persian (fa) localization
 // https://github.com/bgrins/spectrum
 
-(function ($) {
+(function ( $ ) {
 
     var localization = $.spectrum.localization["fa"] = {
         cancelText: "لغو",
-        chooseText: "انتخاب"
+        chooseText: "انتخاب",
+        clearText: "تنظیم مجدد رنگ",
+        noColorSelectedText: "هیچ رنگی انتخاب نشده است!",
+        togglePaletteMoreText: "بیشتر",
+        togglePaletteLessText: "کمتر"
     };
 
     $.extend($.fn.spectrum.defaults, localization);
 
-})(jQuery);
+})( jQuery );
